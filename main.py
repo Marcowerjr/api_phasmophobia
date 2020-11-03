f7b46db2e200a346a8f69c9cfbd83f53cb27a58d
Marcowerjr <marcowerjr@gmail.com>
2020-11-03 09:16:20 -0700

    add authentication

--- main.py
+++ main.py
@@ -11,73 +11,105 @@ app = create_app()
 def test():
     return jsonify({"message":"Testing"})
 
-@app.route('/api/all_ghosts/')
-def show_ghosts():
-    all_ghosts=dumps(list(db.db.phasmophobia.find()))
-    return all_ghosts
-
-
-@app.route('/api/ghost/<int:n_ghost>/', methods=['GET'])
-def show_a_ghost(n_ghost):
-    ghost=dumps(db.db.phasmophobia.find_one({"n_ghost":n_ghost}))
-
-    if ghost != "null":
-        return ghost
+@app.route('/<string:password>/api/all_ghosts/')
+def show_ghosts(password):
+    if password=="phasmophobia":
+        all_ghosts=dumps(list(db.db.phasmophobia.find()))
+        return all_ghosts
     else:
         return jsonify({
-                "status":404,
-                "message":"Tipo de fantasma no existente",
-            })
+            "status":404,
+            "message":"Contraseña errónea"
+        })
+    
+
+
+@app.route('/<string:password>/api/ghost/<int:n_ghost>/', methods=['GET'])
+def show_a_ghost(n_ghost, password):
+    if password=="phasmophobia":
+        ghost=dumps(db.db.phasmophobia.find_one({"n_ghost":n_ghost}))
+
+        if ghost != "null":
+            return ghost
+        else:
+            return jsonify({
+                    "status":404,
+                    "message":"Tipo de fantasma no existente",
+                })
+    else:
+        return jsonify({
+            "status":404,
+            "message":"Contraseña errónea"
+        })
+    
             
 
 
-@app.route('/api/add_ghost/', methods=['POST'])
-def add_new_ghost():
-    if len(request.json) == 6:
-        db.db.phasmophobia.insert_one({
-            "n_ghost":request.json["n_ghost"],
-            "tipo":request.json["tipo"],
-            "description":request.json["description"],
-            "puntos_fuertes":request.json["puntos_fuertes"],
-            "puntos_debiles":request.json["puntos_debiles"],
-            "pruebas_para_detectarlo":request.json["pruebas_para_detectarlo"],
+@app.route('/<string:password>/api/add_ghost/', methods=['POST'])
+def add_new_ghost(password):
+    if password=="phasmophobia":
+        if len(request.json) == 6:
+            db.db.phasmophobia.insert_one({
+                "n_ghost":request.json["n_ghost"],
+                "tipo":request.json["tipo"],
+                "description":request.json["description"],
+                "puntos_fuertes":request.json["puntos_fuertes"],
+                "puntos_debiles":request.json["puntos_debiles"],
+                "pruebas_para_detectarlo":request.json["pruebas_para_detectarlo"],
+            })
+        else:
+            return jsonify({
+                "ERROR":"ERROR",
+                "message":"Te faltan datos",
+            })
+
+        return jsonify({
+            "message":f"El fantasma de tipo {request.json['tipo']} se ha añadido satisfactoriamente",
+            "status":200,
         })
     else:
         return jsonify({
-            "ERROR":"ERROR",
-            "message":"Te faltan datos",
+            "status":404,
+            "message":"Contraseña errónea"
         })
-    
-    return jsonify({
-        "message":f"El fantasma de tipo {request.json['tipo']} se ha añadido satisfactoriamente",
-        "status":200,
-    })
 
-@app.route('/api/ghost/update/<int:n_ghost>/', methods=['PUT'])
-def update_ghost(n_top):
-    if db.db.phasmophobia.find_one({'n_ghost' : request.json["n_ghost"]}):
-        db.db.phasmophobia.update_one({'n_ghost' : request.json["n_ghost"]},
-        {'$set':{
-            "n_ghost":request.json["n_ghost"],
-            "tipo":request.json["tipo"],
-            "description":request.json["description"],
-            "puntos_fuertes":request.json["puntos_fuertes"],
-            "puntos_debiles":request.json["puntos_debiles"],
-            "pruebas_para_detectarlo":request.json["pruebas_para_detectarlo"],
-        }})
+@app.route('/<string:password>/api/ghost/update/<int:n_ghost>/', methods=['PUT'])
+def update_ghost(n_top, password):
+    if password=="phasmophobia":
+        if db.db.phasmophobia.find_one({'n_ghost' : request.json["n_ghost"]}):
+            db.db.phasmophobia.update_one({'n_ghost' : request.json["n_ghost"]},
+            {'$set':{
+                "n_ghost":request.json["n_ghost"],
+                "tipo":request.json["tipo"],
+                "description":request.json["description"],
+                "puntos_fuertes":request.json["puntos_fuertes"],
+                "puntos_debiles":request.json["puntos_debiles"],
+                "pruebas_para_detectarlo":request.json["pruebas_para_detectarlo"],
+            }})
+        else:
+            return jsonify({'status':400, "message": f"Fantasma de tipo {request.json['tipo']} no existe"})
+
+        return jsonify({'status':200, "message": f"El fantasma de tipo {request.json['tipo']} fue actualizado"})
     else:
-        return jsonify({'status':400, "message": f"Fantasma de tipo {request.json['tipo']} no existe"})
+        return jsonify({
+            "status":404,
+            "message":"Contraseña errónea"
+        })
 
-    return jsonify({'status':200, "message": f"El fantasma de tipo {request.json['tipo']} fue actualizado"})
+@app.route('/<string:password>/api/ghost/delete/<int:n_ghost>/', methods=['DELETE'])
+def delete_song(n_top, password):
+    if password=="phasmophobia":
+        if db.db.phasmophobia.find_one({'n_ghost' : request.json["n_ghost"]}):
+            db.db.phasmophobia.delete_one({'n_ghost' : request.json["n_ghost"]})
+        else:
+            return jsonify({'status':400, "message": f"Fantasma de tipo {request.json['tipo']} no existe"})
 
-@app.route('/api/ghost/delete/<int:n_ghost>/', methods=['DELETE'])
-def delete_song(n_top):
-    if db.db.phasmophobia.find_one({'n_ghost' : request.json["n_ghost"]}):
-        db.db.phasmophobia.delete_one({'n_ghost' : request.json["n_ghost"]})
+        return jsonify({"status":200, "message": f"El fantasma de tipo {request.json['tipo']} fue eliminado"})
     else:
-        return jsonify({'status':400, "message": f"Fantasma de tipo {request.json['tipo']} no existe"})
-
-    return jsonify({"status":200, "message": f"El fantasma de tipo {request.json['tipo']} fue eliminado"})
+        return jsonify({
+            "status":404,
+            "message":"Contraseña errónea"
+        })
 
 
 if __name__ == "__main__":
